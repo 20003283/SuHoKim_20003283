b737bd6c03e10c8f23b8d74b08e4f202bd0ee9d9
20003283 <133726968+20003283@users.noreply.github.com>
2023-05-23 10:47:06 +0900

    Update PQW2ECI.m

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -15,4 +15,4 @@ R3= [ cos(-RAAN) sin(-RAAN) 0; -sin(RAAN) cos(-RAAN) 0; 0 0 1];
 R1= [ 1 0 0; 0 cos(-inc_angle) sin(-inc_angle); 0 -sin(-inc_angle) cos(-inc_angle)];
 R3_dot= [ cos(-arg_prg) sin(-arg_prg) 0; -sin(-arg_prg) cos(-arg_prg) 0; 0 0 1];
 
-C=R3*R1*R3_dot;
+C=(R3*R1*R3_dot)';
